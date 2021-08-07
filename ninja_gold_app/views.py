e03f2de183bea61b18e600f38ca8634dd907901a
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2021-08-06 20:16:15 -0600

    First Commit

---
+++ ninja_gold_app/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect, HttpResponse
+import random
+from time import gmtime, strftime
+
+
+def index(request):
+    if 'gold_amt' and 'activity' not in request.session:
+        request.session['gold_amt'] = 0
+        request.session['activity'] = []
+    return render(request, 'index.html')
+
+def process_money(request):
+    location = request.POST['location']
+    date_time = strftime("%A, %B %d, %Y at %I:%M %p", gmtime())
+    
+    gold = {
+        'farm':random.randint(10,20),
+        'cave': random.randint(5,10),
+        'house':random.randint(2,5),
+        'casino':random.randint(-50,50),
+    }
+    
+    if request.method == "POST":
+        request.session['gold_amt'] += gold[location]
+        if gold[location] < 0:
+            message = f"You earned {gold[location]} gold from the {location} on {date_time}. You should stop gambling!"
+
+        else:
+            message = f"You earned {gold[location]} gold from the {location} on {date_time}. You should stop gambling!"
+
+        request.session['activity'].append(message)
+    else:
+        request.session.flush()
+    return redirect("/") 
+
+def reset(request):
+    request.session.flush()
+    return redirect("/")
